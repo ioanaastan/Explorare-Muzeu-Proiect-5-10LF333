f2724cdea093def83e96faffb2063c787b6a4485
shadykun <soltuzraresflorin2004@gmail.com>
2024-12-01 18:32:36 +0200

    Move Shader class in its own file

---
+++ Explorare-Muzeu-Proiect-5-10LF333/Shader.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <GL/glew.h>
+#include <string>
+#include <GLM.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+class Shader
+{
+public:
+	Shader(const char* vertexPath, const char* fragmentPath);
+
+	~Shader();
+
+	void Use() const;
+
+	unsigned int GetID() const;
+
+	unsigned int loc_model_matrix;
+	unsigned int loc_view_matrix;
+	unsigned int loc_projection_matrix;
+
+	void SetVec3(const std::string& name, const glm::vec3& value) const;
+	void SetVec3(const std::string& name, float x, float y, float z) const;
+	void SetFloat(const std::string& name, float fValue) const;
+	void SetInt(const std::string& name, float fValue) const;
+	void SetMat4(const std::string& name, const glm::mat4& mat) const;
+
+private:
+	void Init(const char* vertexPath, const char* fragmentPath);
+
+	void CheckCompileErrors(unsigned int shader, std::string type);
+
+private:
+	unsigned int ID;
+};
