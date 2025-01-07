b0f9b20bc39ba638e8ee8d6d558f15da67cf08c0
shadykun <soltuzraresflorin2004@gmail.com>
2025-01-07 17:24:07 +0200

    shifty

--- Explorare-Muzeu-Proiect-5-10LF333/Explorare-Muzeu-Proiect-5-10LF333.cpp
+++ Explorare-Muzeu-Proiect-5-10LF333/Explorare-Muzeu-Proiect-5-10LF333.cpp
@@ -108,106 +108,6 @@ double deltaTime = 0.0f;
 double lastFrame = 0.0f;
 
 
-//GLuint skyboxVAO, skyboxVBO;
-//Shader skyboxShader;
-//CubemapTexture skybox;
-//
-//void InitSkybox() {
-//
-//	float skyboxVertices[] = {
-//		-1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f,  1.0f, -1.0f,
-//		1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f,  1.0f, -1.0f, -1.0f, -1.0f,
-//		-1.0f,  1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,
-//		1.0f, -1.0f,  1.0f, 1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f,
-//		-1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,  1.0f,  1.0f,  1.0f,
-//		1.0f,  1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f
-//	};
-//
-//
-//	glGenVertexArrays(1, &skyboxVAO);
-//	glGenBuffers(1, &skyboxVBO);
-//
-//	glBindVertexArray(skyboxVAO);
-//
-//	glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
-//	glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
-//
-//	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-//	glEnableVertexAttribArray(0);
-//
-//	glBindBuffer(GL_ARRAY_BUFFER, 0);
-//
-//	glBindVertexArray(0);
-//}
-//
-//void RenderSkybox() {
-//	glBindVertexArray(skyboxVAO);
-//	glDrawArrays(GL_TRIANGLES, 0, 36);
-//	glBindVertexArray(0);
-//}
-//
-//class CubemapTexture {
-//public:
-//	CubemapTexture(const std::string& Directory,
-//		const std::string& PosXFilename,
-//		const std::string& NegXFilename,
-//		const std::string& PosYFilename,
-//		const std::string& NegYFilename,
-//		const std::string& PosZFilename,
-//		const std::string& NegZFilename) {
-//		m_fileNames[0] = Directory + "\\Models\\Skybox\\Box_Right";
-//		m_fileNames[1] = Directory + "\\Models\\Skybox\\Box_Left";
-//		m_fileNames[2] = Directory + "\\Models\\Skybox\\Box_Top";
-//		m_fileNames[3] = Directory + "\\Models\\Skybox\\Box_Bottom";
-//		m_fileNames[4] = Directory + "\\Models\\Skybox\\Box_Back";
-//		m_fileNames[5] = Directory + "\\Models\\Skybox\\Box_Front";
-//	}
-//
-//	~CubemapTexture() {
-//		glDeleteTextures(1, &m_textureObj);
-//	}
-//
-//	bool Load() {
-//		glGenTextures(1, &m_textureObj);
-//		glBindTexture(GL_TEXTURE_CUBE_MAP, m_textureObj);
-//
-//		for (unsigned int i = 0; i < 6; i++) {
-//			int width, height, channels;
-//			unsigned char* data = stbi_load(m_fileNames[i].c_str(), &width, &height, &channels, STBI_rgb_alpha);
-//
-//			if (data == nullptr) {
-//				std::cout << "Error loading texture '" << m_fileNames[i] << "': " << stbi_failure_reason() << std::endl;
-//				return false;
-//			}
-//
-//			glTexImage2D(types[i], 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-//			stbi_image_free(data);
-//		}
-//
-//		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-//		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-//		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-//		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-//
-//		return true;
-//	}
-//	void Bind(GLenum TextureUnit) {
-//		glActiveTexture(TextureUnit);
-//		glBindTexture(GL_TEXTURE_CUBE_MAP, m_textureObj);
-//	}
-//
-//private:
-//	GLuint m_textureObj;
-//	std::string m_fileNames[6];
-//	GLenum types[6] = {
-//		GL_TEXTURE_CUBE_MAP_POSITIVE_X, GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
-//		GL_TEXTURE_CUBE_MAP_POSITIVE_Y, GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
-//		GL_TEXTURE_CUBE_MAP_POSITIVE_Z, GL_TEXTURE_CUBE_MAP_NEGATIVE_Z
-//	};
-//};
-//
-
 void CreateVBO()
 {
 	// indexurile cubului
@@ -380,14 +280,6 @@ void RenderFrame()
 	view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	glUniformMatrix4fv(ViewMatrixLocation, 1, GL_FALSE, glm::value_ptr(view));*/
 
-	//for (unsigned int i = 0; i < sizeof(cubePositions) / sizeof(cubePositions[0]); i++) {
-	//	// calculate the model matrix for each object and pass it to shader before drawing
-	//	glm::mat4 worldTransf = glm::translate(glm::mat4(1.0), cubePositions[i]);
-	//	ShaderProgram.SetMat4("WorldMatrix", worldTransf);
-
-	//	RenderCube();
-	//}
-
 	//Here we render the models
 
 	objShader.Use();
@@ -437,13 +329,12 @@ void RenderFrame()
 	objShader.SetMat4("model", wolfModel);
 	models[3].Draw(objShader);
 
-//<<<<<<< HEAD
-	
+
 	glm::mat4 treeModel = glm::mat4(1.0);
-treeModel = glm::translate(treeModel, glm::vec3(2.f, -1.f, 2.5f));
-treeModel = glm::scale(treeModel, glm::vec3(0.2f));
-objShader.SetMat4("model", treeModel);
-models[4].Draw(objShader);
+	treeModel = glm::translate(treeModel, glm::vec3(2.f, -1.f, 2.5f));
+	treeModel = glm::scale(treeModel, glm::vec3(0.2f));
+	objShader.SetMat4("model", treeModel);
+	models[4].Draw(objShader);
 
 
 	for (int i = 0; i < 10; i++) { // Place 10 instances
@@ -472,12 +363,13 @@ models[4].Draw(objShader);
 	objShader.SetMat4("model", catModel);
 	models[6].Draw(objShader);
 
+
 	glm::mat4 tree2Model = glm::mat4(1.0);
 	tree2Model = glm::translate(tree2Model, glm::vec3(0.f, -1.f, 2.5f));
 	//tree2Model = glm::scale(tree2Model, glm::vec3(0.3f));
 	objShader.SetMat4("model", tree2Model);
 	models[7].Draw(objShader);
-	//Here we render the light source
+
 
 	glm::mat4 tree3Model = glm::mat4(1.0);
 	tree3Model = glm::translate(tree3Model, glm::vec3(0.f, -3.f, 0.f));
@@ -648,12 +540,12 @@ models[4].Draw(objShader);
 	models[23].Draw(objShader);
 
 
-	//glm::mat4 WallModel = glm::mat4(1.0);
-	//WallModel = glm::translate(WallModel, glm::vec3(8.0f, 0.0f, 10.0f));
-	//WallModel = glm::rotate(WallModel, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-	//WallModel = glm::scale(WallModel, glm::vec3(4.f));
-	//objShader.SetMat4("model", WallModel);
-	//models[25].Draw(objShader);
+	glm::mat4 WallModel = glm::mat4(1.0);
+	WallModel = glm::translate(WallModel, glm::vec3(8.0f, 0.0f, 10.0f));
+	WallModel = glm::rotate(WallModel, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	WallModel = glm::scale(WallModel, glm::vec3(4.f));
+	objShader.SetMat4("model", WallModel);
+	models[24].Draw(objShader);
 
 
 
@@ -696,7 +588,7 @@ models[4].Draw(objShader);
 	// Rotate it to face the right direction
 	butterflyModel = glm::rotate(butterflyModel, glm::radians(270.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 	objShader.SetMat4("model", butterflyModel);
-	models[models.size() - 1].Draw(objShader);
+	models[models.size() - 2].Draw(objShader);
 
 	// ********************************************************************************************************************
 
@@ -956,6 +848,9 @@ void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+		deltaTime *= 2;
 
 	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
 		pCamera->ProcessKeyboard(FORWARD, (float)deltaTime);
